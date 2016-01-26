152610619b7332b46a4e96a8ea16a73f504ad9a6
Stefan Müller <mueller@cos-gbr.de>
2016-01-26 23:26:37 +0100

    speed up arduino

--- AmbiBeamArduino/AmbiBeamArduino.ino
+++ AmbiBeamArduino/AmbiBeamArduino.ino
@@ -8,9 +8,10 @@
 #define LED 13
 
 
-#define SOH 1
-#define STX 2
-#define ETX 3
+#define SOH 0x01
+#define STX 0x02
+#define ETX 0x03
+#define ACK 0x06
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
@@ -25,6 +26,7 @@ unsigned char state = 0;
 uint16_t len = 0;
 unsigned char brightness = 254;
 uint16_t chunkPos = 0;
+unsigned long  timeout;
 
 void loop() { 
   char c = 0;
@@ -32,6 +34,7 @@ void loop() {
   if(Serial.available())
     switch(state){
       case 0:
+        timeout = millis();
         c = Serial.read();
         if ( c == SOH)
         {
@@ -41,7 +44,7 @@ void loop() {
         }
         break;
       case 1:
-       Serial.readBytes((char*)&len, 2); 
+        Serial.readBytes((char*)&len, 2); 
         
         state++;
 
@@ -65,14 +68,16 @@ void loop() {
         c = Serial.read();
         if(c == STX)
         {
+          timeout = millis();
           chunkPos = 0;
+          //FastLED.clear();
           state++;
           //Serial.println(F("STX"));
         }
         break;
 
       case 4:
-        FastLED.clear();
+        timeout = millis();
         chunkPos += Serial.readBytes((char*)leds, len * 3 - chunkPos);
         if (chunkPos >= (len * 3))
         {
@@ -90,7 +95,13 @@ void loop() {
           FastLED.show();      
           state = 0;
           //Serial.println(F("ETX"));
+          Serial.write(ACK);
           break;
         }   
+    }
+    if(millis() - timeout > 250)
+    {
+      //Serial.println(F("Timeout"));
+      state = 0;
     }
 }
