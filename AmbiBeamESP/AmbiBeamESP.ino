dfb6717c18a50382c3aa83e6077a22c0ef020955
Stefan Müller <stefan.mueller@uni-tuebingen.de>
2016-02-02 11:47:06 +0100

    extended esp to ambibeam packet format

--- AmbiBeamESP/AmbiBeamESP.ino
+++ AmbiBeamESP/AmbiBeamESP.ino
@@ -1,3 +1,5 @@
+#include <FastLED.h>
+
 // NeoPixelBus
 
 
@@ -14,11 +16,28 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
+#include <math.h>
 
+// How many leds in your strip?
+#define NUM_LEDS 440
+// Where is it connected ?
+#define DATA_PIN 3
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
 #define localPort 2222
+#define SOH 0x01
+#define STX 0x02
+#define ETX 0x03
+#define ACK 0x06
 
 WiFiUDP Udp;
+unsigned char state = 0;
+uint16_t len = 0;
+unsigned char brightness = 254;
+uint16_t chunkPos = 0;
+unsigned long  timeout;
 
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println(F("Entered config mode"));
@@ -35,7 +54,9 @@ void setup() {
   WiFiManager wifiManager;
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect("ESP", "test");
-    
+
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
   Udp.begin(localPort);
 }
 
@@ -49,13 +70,55 @@ int packetSize = Udp.parsePacket();
     Serial.print(remoteIp);
     Serial.print(", port ");
     Serial.println(Udp.remotePort());
-
+    
     // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 255);
-    if (len > 0) {
-      packetBuffer[len] = 0;
+    uint16_t packetLen = Udp.read(packetBuffer, 255);
+    if (packetLen > 0) {
+      packetBuffer[packetLen] = 0;
+  
+      Serial.println("Contents:");
+      Serial.println(packetBuffer);
+      char* p = &packetBuffer[0];
+      switch(state){
+          case 0: {
+            timeout = millis();
+            if(*(p++) == SOH)
+            {
+              len = (uint16_t) *(p++) << 8 | *(p++);
+              brightness = *(p++);
+              if(*(p++) == STX){
+                FastLED.setBrightness(brightness);
+                chunkPos = 0;
+                state++;
+              } else { break; }
+            } else { break; } 
+          }
+        case 1: {
+          timeout = millis();
+          int bytesToCopy = std::min(len * 3, packetLen - 5);
+          char* pleds = (char*)leds;
+          pleds += chunkPos / 3;
+          memcpy ( pleds, p, bytesToCopy);
+          if (chunkPos + bytesToCopy >= (len * 3))
+          {
+            state++;
+          } else {
+            chunkPos += bytesToCopy;
+          }
+          break;
+        }
+        case 2:
+        {
+          FastLED.show();      
+          state = 0;
+          break;
+        }
+      }
     }
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
   }
+  if(millis() - timeout > 250)
+    {
+      //Serial.println(F("Timeout"));
+      state = 0;
+    }
 }
